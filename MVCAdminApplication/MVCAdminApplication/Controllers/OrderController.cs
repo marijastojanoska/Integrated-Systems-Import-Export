f9e0a23d85074272d610d765edce8f0461709f93
Marija Stojanoska <marijastojanoska2002@gmail.com>
2024-05-25 15:03:26 +0200

    initial

---
+++ MVCAdminApplication/MVCAdminApplication/Controllers/OrderController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAdminApplication.Models;
+using Newtonsoft.Json;
+using System.Text;
+using ClosedXML.Excel;
+using GemBox.Document;
+using System.Reflection;
+
+namespace MVCAdminApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        public OrderController()
+        {
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            string URL = "http://localhost:5054/api/Admin/GetAllOrders";
+            HttpResponseMessage response = client.GetAsync(URL).Result;
+
+            var data = response.Content.ReadAsAsync<List<Order>>().Result;
+            return View(data);
+        }
+
+        public IActionResult Details(Guid Id)
+        {
+            HttpClient client = new HttpClient();
+            string URL = "http://localhost:5054/api/Admin/GetDetailsForOrder";
+            var model = new
+            {
+                Id = Id
+            };
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PostAsync(URL, content).Result;
+
+            var data = response.Content.ReadAsAsync<Order>().Result;
+            return View(data);
+        }
+
+        public FileContentResult CreateInvoice(Guid Id)
+        {
+            HttpClient client = new HttpClient();
+            string URL = "http://localhost:5054/api/Admin/GetDetailsForOrder";
+            var model = new
+            {
+                Id = Id
+            };
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PostAsync(URL, content).Result;
+
+            var data = response.Content.ReadAsAsync<Order>().Result;
+
+            var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Invoice.docx");
+            var document = DocumentModel.Load(templatePath);
+            document.Content.Replace("{{OrderNumber}}", data.Id.ToString());
+            document.Content.Replace("{{FirstName}}", data.Owner.FirstName);
+            StringBuilder sb = new StringBuilder();
+            double total = 0.0;
+            foreach (var item in data.ProductInOrders)
+            {
+                sb.Append("TicketId " + item.TicketId + " with quantity " + item.Quantity + " with price " + item.OrderedProduct.Price + "$\n");
+                total += (item.Quantity * item.OrderedProduct.Price);
+            }
+            document.Content.Replace("{{ProductList}}", sb.ToString());
+            document.Content.Replace("{{TotalPrice}}", total.ToString() + "$");
+
+            var stream = new MemoryStream();
+            document.Save(stream, new PdfSaveOptions());
+            return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportedInvoice.pdf");
+        }
+
+
+        [HttpGet]
+        public FileContentResult ExportOrders()
+        {
+            string fileName = "AllOrders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var workBook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workBook.Worksheets.Add("Orders");
+
+                worksheet.Cell(1, 1).Value = "Order ID";
+                worksheet.Cell(1, 2).Value = "Customer First Name";
+
+                HttpClient client = new HttpClient();
+                string URL = "http://localhost:5054/api/Admin/GetAllOrders";
+                HttpResponseMessage response = client.GetAsync(URL).Result;
+
+                var data = response.Content.ReadAsAsync<List<Order>>().Result;
+
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    var order = data[i];
+                    worksheet.Cell(i + 2, 1).Value = order.Id.ToString();
+                    worksheet.Cell(i + 2, 2).Value = order.Owner.FirstName;
+
+                    for (int j = 0; j < order.ProductInOrders.Count(); j++)
+                    {
+                        worksheet.Cell(1, j + 3).Value = "Ticket - " + (j + 1);
+                        worksheet.Cell(i + 2, j + 3).Value = order.ProductInOrders.ElementAt(j).TicketId.ToString();
+
+                    }
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, contentType, fileName);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ integrated_systems/EShop.Web/Controllers/API/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using EShop.Domain.Domain;
+using EShop.Domain.DTO;
+using EShop.Domain.Identity;
+using EShop.Repository;
+using EShop.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Movie_App.Service.Interface;
+
+namespace EShop.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMovieService _movieService;
+        public AdminController(IOrderService orderService, IMovieService movieService)
+        {
+            _orderService = orderService;
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public List<Order> Index()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+        [HttpGet("[action]")]
+        public List<Order> GetAllOrders()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+        [HttpPost("[action]")]
+        public Order GetDetailsForOrder(BaseEntity model)
+        {
+            return _orderService.GetDetailsForOrder(model);
+        }
+
+        [HttpPost("[action]")]
+        public bool ImportAllMovies(List<MovieDTO> model)
+        {
+            foreach (var movie in model)
+            {                
+                var neww = new Movie
+                {
+                    Id = Guid.NewGuid(),
+                    MovieName = movie.MovieName,
+                    MovieDescription = movie.MovieDescription,
+                    MovieImage = movie.MovieImage,
+                    Rating = movie.Rating
+                };
+                _movieService.CreateNewMovie(neww);
+            }
+            return true;
+        }
+    }
+}
